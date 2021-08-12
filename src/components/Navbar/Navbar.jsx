88b74bef7f181b242bd55bba95e2cd583394bb0d
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-08-12 14:31:09 -0400

    added Navbar and default file for appp component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core'
+import { ShoppingCart } from '@material-ui/icons'
+import Logo from "../../assets/shop.png"
+import useStyles from './styles'
+
+const Navbar = () => {
+    const classes = useStyles()
+    return (
+        <>
+            <AppBar position="fixed" className={classes.appBar} color="inherit">
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title} color="inherit">
+                        <img src={Logo} alt="Commerce.js" height="25px" className={classes.image} />
+                        The Shop
+                    </Typography>
+                    <div className={classes.grow}/>
+                    <div className={classes.button}>
+                        <IconButton aria-label="Show cart items" color="inherit">
+                            <Badge badgeContent={2} color="secondary">
+                                <ShoppingCart/>
+                            </Badge>
+                        </IconButton>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </>
+    )
+}
+
+export default Navbar
