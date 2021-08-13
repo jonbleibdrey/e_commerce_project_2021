11571307d17e53e2ca45ee2c1c95d620e8fbe43d
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-08-13 11:24:58 -0400

    added new data and fetching them instead of hardcoding it

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, {useState, useEffect} from 'react'
-import { Products, Navbar} from './components'
-import { commerce } from './lib/commerce'
+import React, { useState, useEffect } from "react";
+import { Products, Navbar } from "./components";
+import { commerce } from "./lib/commerce";
 
 const App = () => {
-    const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-    const fetchProducts = async () => {
-        const {data} = await commerce.products.list();
-        
-        setProducts(data)
-    }
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
 
-    useEffect(() => {
-        fetchProducts()
-    }, [])
+    setProducts(data);
+  };
 
-    console.log(products)
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
-    return (
-        <div>
-            <Navbar/>
-           <Products/>
-        </div>
-    )
-}
+  console.log(products);
 
-export default App
+  return (
+    <div>
+      <Navbar />
+      <Products products={products}/>
+    </div>
+  );
+};
+
+export default App;
