ff2f7889f571628f7dc6ebfd7c57d49876f2a730
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-08-16 12:06:39 -0400

    added empty card and full cart component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Products, Navbar, Cart} from "./components";
 import { commerce } from "./lib/commerce";
+import { Products, Navbar, Cart } from "./components";
 
 const App = () => {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState({});
-
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
@@ -13,29 +12,26 @@ const App = () => {
     setProducts(data);
   };
 
-  const fetchCart = async () =>{
-      
-
-      setCart(await commerce.cart.retrieve())
-  }
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  };
 
   const handleAddToCart = async (productId, quantity) => {
-        const item = await commerce.cart.add(productId, quantity)
-        setCart(item.cart)
-  }
+    const item = await commerce.cart.add(productId, quantity);
+    setCart(item.cart);
+  };
 
   useEffect(() => {
     fetchProducts();
-    fetchCart()
+    fetchCart();
   }, []);
 
-  console.log("whats in the cart: ",cart);
 
   return (
     <div>
-      <Navbar totalItems={cart.total_items}/>
+      <Navbar totalItems={cart.total_items} />
       {/* <Products products={products} onAddToCart={handleAddToCart}/> */}
-      <Cart cart={cart}/>
+      <Cart cart={cart} />
     </div>
   );
 };
