64de4148ecd13fd1e1a4d5e1eff2775613234c39
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-08-16 11:30:34 -0400

    added Cart component

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {Container, Typography, Button, Grid} from '@material-ui/core'
+import { classes } from 'istanbul-lib-coverage'
+
+
+const Cart = ({cart}) => {
+    const isEmpty = cart.line_items.length === 0
+
+    const EmptyCart = () => (
+        <Typography variant="subtitle1"> You have no items in your shopping cart, start adding some!</Typography>
+    )
+
+    const FilledCart = () => (
+        <>
+            <Grid container  spacing={3}>
+
+            </Grid>
+        </>
+    )
+    // const classes = useStyle()
+
+    return (
+        <Container>
+            <div className={classes.toolbar} />
+            <Typography className={classes.title} variant="h3">
+                Your Shopping Cart
+            </Typography>
+            {isEmpty ? <EmptyCart/> : <FilledCart/>}
+        </Container> 
+    )
+}
+
+export default Cart
