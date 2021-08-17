671b445a0136ac01838b04f9db5de0d09cba6025
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-08-17 19:57:14 -0400

    added the increase,decrease, and remove button functionality

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,7 +4,12 @@ import useStyles from "./styles";
 import CartItem from "./CartItem/CartItem";
 import { Link } from "react-router-dom";
 
-const Cart = ({ cart }) => {
+const Cart = ({
+  cart,
+  handleUpdateCartQty,
+  handleRemoveFromCart,
+  handleEmptyCart,
+}) => {
   // const isEmpty = cart.line_items && cart.line_items.length === 0
   const classes = useStyles();
 
@@ -25,7 +30,11 @@ const Cart = ({ cart }) => {
         {cart.line_items &&
           cart.line_items.map((item) => (
             <Grid item xs={12} sm={4} key={item.id}>
-              <CartItem item={item} />
+              <CartItem
+                item={item}
+                handleUpdateCartQty={handleUpdateCartQty}
+                handleRemoveFromCart={handleRemoveFromCart}
+              />
             </Grid>
           ))}
       </Grid>
@@ -40,6 +49,7 @@ const Cart = ({ cart }) => {
             type="button"
             variant="contained"
             color="secondary"
+            onClick={handleEmptyCart}
           >
             {" "}
             Empty Cart
